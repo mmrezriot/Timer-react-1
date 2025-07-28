8d658cf46f5a25eff399458ff0fa2cf5217cc174
mmrez <mohammdreza.riot7676@gmail.com>
2025-07-28 19:42:08 +0330

    Refactor timer application: update HTML structure for RTL support, enhance CSS styles, and implement timer functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+var interval ;
+
+class App extends React.Component {
+
+  constructor(){
+super();
+this.state= {
+  seconds : 0,
+  minutes : 0,
+  hours : 0,
+  isStart: false
+
+}
+  }
+
+startTimer = ()=> {
+interval = setInterval(() => {
+  this.setState({
+    seconds : this.state.seconds + 1,
+    isStart : true
+  })
+  if(this.state.seconds == 60){
+  this.setState({
+    seconds : 0 , 
+    minutes : this.state.minutes + 1
+  })
+  }
+  if(this.state.minutes == 60){
+  this.setState({
+    minutes : 0 , 
+    hours : this.state.hours + 1
+  })
+  }
+}, 1000);
+ }
+
+
+ stopTimer = ()=> {
+  this.setState({
+    isStart:false
+  })
+  clearInterval(interval);
+ }
+
+
+ resetTimer = ()=> {
+  clearInterval(interval);
+this.setState({
+  seconds : 0,
+  minutes : 0,
+  hours : 0,
+  isStart: false
+})
+
+ }
+
+
+render(){
+
+  let s = this.state.seconds; 
+  let m = this.state.minutes;
+  let h = this.state.hours;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+     <div class="timer-container">
+  
+  <div class="time-segment">
+    <span id="seconds">{s > 9 ? s : "0"+ s}</span>
+    <span class="label">ثانیه</span>
+  </div>
+  <div class="separator">:</div>
+  <div class="time-segment">
+    <span id="minutes">{m > 9 ? m : "0"+ m}</span>
+    <span class="label">دقیقه</span>
+  </div>
+  <div class="separator">:</div>
+  <div class="time-segment">
+    <span id="hours">{h > 9 ? h : "0"+ h}</span>
+    <span class="label">ساعت</span>
+  </div>
+</div>
+
+<div class="buttons-container">
+  <button id="start-btn" onClick={this.startTimer}>شروع</button>
+  <button id="stop-btn" onClick={this.stopTimer}>توقف</button>
+  <button id="reset-btn" onClick={this.resetTimer}>ریست</button>
+</div>
+
     </>
   )
+}
 }
 
 export default App
